995368c384c4a734b9118ab94eb7b62f08bfcd17
espath <espath@gmail.com>
2016-03-01 15:08:01 +0300

    fixing plots

--- mimclib/plot.py
+++ mimclib/plot.py
@@ -1,5 +1,80 @@
 import numpy as np
 
+class StepFunction(object):
+
+    """
+    A basic step function.
+
+    Values at the ends are handled in the simplest way possible:
+    everything to the left of x[0] is set to ival; everything
+    to the right of x[-1] is set to y[-1].
+
+    Parameters
+    ----------
+    x : array-like
+    y : array-like
+    ival : float
+        ival is the value given to the values to the left of x[0]. Default
+        is 0.
+    sorted : bool
+        Default is False.
+    side : {'left', 'right'}, optional
+        Default is 'left'. Defines the shape of the intervals constituting the
+        steps. 'right' correspond to [a, b) intervals and 'left' to (a, b].
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from statsmodels.distributions.empirical_distribution import StepFunction
+    >>>
+    >>> x = np.arange(20)
+    >>> y = np.arange(20)
+    >>> f = StepFunction(x, y)
+    >>>
+    >>> print f(3.2)
+    3.0
+    >>> print f([[3.2,4.5],[24,-3.1]])
+    [[  3.   4.]
+     [ 19.   0.]]
+    >>> f2 = StepFunction(x, y, side='right')
+    >>>
+    >>> print f(3.0)
+    2.0
+    >>> print f2(3.0)
+    3.0
+    """
+
+    def __init__(self, x, y, ival=0., sorted=False, side='left'):
+
+        if side.lower() not in ['right', 'left']:
+            msg = "side can take the values 'right' or 'left'"
+            raise ValueError(msg)
+        self.side = side
+
+        _x = np.asarray(x)
+        _y = np.asarray(y)
+
+        if _x.shape != _y.shape:
+            msg = "x and y do not have the same shape"
+            raise ValueError(msg)
+        if len(_x.shape) != 1:
+            msg = 'x and y must be 1-dimensional'
+            raise ValueError(msg)
+
+        self.x = np.r_[-np.inf, _x]
+        self.y = np.r_[ival, _y]
+
+        if not sorted:
+            asort = np.argsort(self.x)
+            self.x = np.take(self.x, asort, 0)
+            self.y = np.take(self.y, asort, 0)
+        self.n = self.x.shape[0]
+
+    def __call__(self, time):
+
+        tind = np.searchsorted(self.x, time, self.side) - 1
+        return self.y[tind]
+
 class ECDF(StepFunction):
 
     """
@@ -88,7 +163,8 @@ ax is in instance of matplotlib.axes
     El = psums[:, 0]/M
     Vl = psums[:, 1]/M - El**2
     return ax.errorbar(np.arange(0, maxL), El,
-                       yerr=3*np.sqrt(Vl/M), *args, **kwargs)
+                       yerr=[3*np.sqrt(Vl/M)-Vl, Vl-3*np.sqrt(Vl/M)],
+                       *args, **kwargs)
 
 def plotTimeVsLvls(ax, runs_data, *args, **kwargs):
     """Plots Time vs TOL of @runs_data, as
@@ -118,10 +194,10 @@ ax is in instance of matplotlib.axes
     real_time = False
     if "real_time" in kwargs:
         real_time = kwargs["real_time"]
-    if real_time:
-        TotalTime = [np.sum(r.totalTime) for r in runs_data]
-        minTime = np.min([r.totalTime for r in runs_data])
-        maxTime = np.max([r.totalTime for r in runs_data])
+        if real_time:
+            TotalTime = [np.sum(r.totalTime) for r in runs_data]
+            minTime = np.min([r.totalTime for r in runs_data])
+            maxTime = np.max([r.totalTime for r in runs_data])
     else:
         TotalTime = [np.sum(r.run.data.t) for r in runs_data]
         minTime = np.min([r.run.data.t for r in runs_data])
@@ -132,8 +208,10 @@ ax is in instance of matplotlib.axes
     ax.set_yscale('log')
     ax.set_xlabel('TOL')
     ax.set_ylabel('Time (s)')
-    return ax.plot(TOL, TotalTime/N,
-                   yerr=(maxTime-minTime), *args, **kwargs)
+    return ax.errorbar(TOL, TotalTime/N,
+                       yerr=[maxTime-TotalTime/N, 
+                             TotalTime/N-minTime],
+                       *args, **kwargs)
 
 def plotLvlsVsTOL(ax, runs_data, *args, **kwargs):
     """Plots L vs TOL of @runs_data, as
@@ -149,7 +227,7 @@ ax is in instance of matplotlib.axes
     ax.set_xscale('log')
     return ax.scatter(TOL, L, *args, **kwargs)
   
-def plotErrorsQQ(ax, runs_data, *args, **kwargs) #(runs, tol, marker='o', color="b", fig=None, label=None):
+def plotErrorsQQ(ax, runs_data, *args, **kwargs): #(runs, tol, marker='o', color="b", fig=None, label=None):
     """Plots Normal vs Empirical CDF of @runs_data, as
 returned by MIMCDatabase.readRunData()
 ax is in instance of matplotlib.axes
