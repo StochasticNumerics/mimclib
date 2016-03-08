d9711358ca26c13b1882b133f0d59a62ba3d638c
Abdo Haji-Ali <abdo.haji.ali@gmail.com>
2016-03-08 16:06:57 +0300

    Modified the plotting code to take more arguments

--- tests/gbm/plot_data.py
+++ tests/gbm/plot_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import mimclib.db as mimcdb
 import mimclib.plot as miplot
+import numpy as np
 
 import warnings
 from matplotlib.cbook import MatplotlibDeprecationWarning
@@ -8,12 +9,17 @@ warnings.simplefilter('ignore', MatplotlibDeprecationWarning)
 
 def addExtraArguments(parser):
     parser.register('type', 'bool', lambda v: v.lower() in ("yes", "true", "t", "1"))
+    parser.add_argument("-db_name", type=str, default='mimc',
+                        action="store", help="Database Name")
     parser.add_argument("-db_user", type=str, default=None,
                         action="store", help="Database User")
     parser.add_argument("-db_host", type=str, default='localhost',
                         action="store", help="Database Host")
     parser.add_argument("-db_tag", type=str, default="NoTag",
                         action="store", help="Database Tag")
+    parser.add_argument("-qoi_exact", type=float, default=np.exp(1.),
+                        action="store", help="Exact value")
+
     parser.add_argument("-o", type=str, default="out.pdf",
                         action="store", help="Output file")
 
@@ -23,16 +29,16 @@ def main():
     parser = argparse.ArgumentParser(add_help=True)
     addExtraArguments(parser)
     args = parser.parse_known_args()[0]
-    db = mimcdb.MIMCDatabase(user=args.db_user,
+    db = mimcdb.MIMCDatabase(db=args.db_name,
+                             user=args.db_user,
                              host=args.db_host)
 
-    import numpy as np
     run_data = db.readRunData(db.getRunDataIDs(tag=args.db_tag, done_flag=[1]))
     run_data = [d for d in run_data if d.iteration_index+1 ==
                 d.total_iterations]
     if len(run_data) == 0:
         raise Exception("No runs!!!")
-    miplot.genPDFBooklet(args.o, run_data, exact=np.exp(1.))
+    miplot.genPDFBooklet(args.o, run_data, exact=args.qoi_exact)
 
 # import mimclib.db as mimcdb
 # import mimclib.plot as miplot
