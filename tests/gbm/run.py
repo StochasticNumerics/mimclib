37b3ca940d2878dbe1d0712334ae521c9ba2b684
Abdo Haji-Ali <abdo.haji.ali@gmail.com>
2016-03-25 14:15:08 +0300

    Added a generic function for a standard test. It's elegant, but I am not sure if I will keep it

--- tests/gbm/run.py
+++ tests/gbm/run.py
@@ -5,8 +5,6 @@ from __future__ import print_function
 import warnings
 import os.path
 import numpy as np
-import mimclib.mimc as mimc
-import mimclib.db as mimcdb
 
 warnings.formatwarning = lambda msg, cat, filename, lineno, line: \
                          "{}:{}: ({}) {}\n".format(os.path.basename(filename),
@@ -15,16 +13,6 @@ warnings.formatwarning = lambda msg, cat, filename, lineno, line: \
 
 
 def addExtraArguments(parser):
-    parser.register('type', 'bool',
-                    lambda v: v.lower() in ("yes", "true", "t", "1"))
-    parser.add_argument("-db_user", type=str,
-                        action="store", help="Database User")
-    parser.add_argument("-db_host", type=str, default='localhost',
-                        action="store", help="Database Host")
-    parser.add_argument("-db_tag", type=str, default="NoTag",
-                        action="store", help="Database Tag")
-    parser.add_argument("-db", type='bool', default=False,
-                        action="store", help="Save in Database")
     parser.add_argument("-qoi_sigma", type=float, default=1.,
                         action="store", help="Volatility in GBM")
     parser.add_argument("-qoi_mu", type=float, default=1.,
@@ -33,43 +21,6 @@ def addExtraArguments(parser):
                         action="store", help="Final time in GBM")
     parser.add_argument("-qoi_S0", type=float, default=1.,
                         action="store", help="Initial condition in GBM")
-    parser.add_argument("-qoi_seed", type=int, default=-1,
-                        action="store", help="Seed for random generator")
-
-
-def main():
-    import argparse
-    parser = argparse.ArgumentParser(add_help=True)
-    addExtraArguments(parser)
-    mimc.MIMCRun.addOptionsToParser(parser)
-    mimcRun = mimc.MIMCRun(**vars(parser.parse_known_args()[0]))
-    if mimcRun.params.qoi_seed >= 0:
-        np.random.seed(mimcRun.params.qoi_seed)
-
-    fnItrDone = None
-    if mimcRun.params.db:
-        if hasattr(mimcRun.params, "db_user"):
-            db = mimcdb.MIMCDatabase(user=mimcRun.params.db_user,
-                                     host=mimcRun.params.db_host)
-        else:
-            db = mimcdb.MIMCDatabase(host=mimcRun.params.db_host)
-        run_id = db.createRun(mimc_run=mimcRun,
-                              tag=mimcRun.params.db_tag)
-        fnItrDone = lambda *a: db.writeRunData(run_id, mimcRun, *a)
-
-    mimcRun.setFunctions(fnSampleQoI=lambda *a: mySampleQoI(mimcRun, *a),
-                         fnItrDone=fnItrDone)
-
-    try:
-        mimcRun.doRun()
-    except:
-        if mimcRun.params.db:
-            db.markRunFailed(run_id)
-        raise   # If you don't want to raise, make sure the following code is not executed
-
-    if mimcRun.params.db:
-            db.markRunSuccessful(run_id)
-    return mimcRun.data.calcEg()
 
 try:
     # Try to import the DLL version of wcumsum,
@@ -117,4 +68,6 @@ def mySampleQoI(run, inds):
     return solves
 
 if __name__ == "__main__":
-    main()
+    import mimclib.test
+    mimclib.test.RunStandardTest(fnSampleQoI=mySampleQoI,
+                                 fnAddExtraArgs=addExtraArguments)
--- tests/pde/run.py
+++ tests/pde/run.py
@@ -1,68 +1,4 @@
-import warnings
-import os.path
 import numpy as np
-import mimclib.mimc as mimc
-import mimclib.db as mimcdb
-
-warnings.formatwarning = lambda msg, cat, filename, lineno, line: \
-                         "{}:{}: ({}) {}\n".format(os.path.basename(filename),
-                                                   lineno, cat.__name__, msg)
-warnings.filterwarnings('error')
-
-
-def addExtraArguments(parser):
-    parser.register('type', 'bool',
-                    lambda v: v.lower() in ("yes", "true", "t", "1"))
-    parser.add_argument("-db_user", type=str,
-                        action="store", help="Database User")
-    parser.add_argument("-db_host", type=str, default='localhost',
-                        action="store", help="Database Host")
-    parser.add_argument("-db_tag", type=str, default="NoTag",
-                        action="store", help="Database Tag")
-    parser.add_argument("-db", type='bool', default=False,
-                        action="store", help="Save in Database")
-    parser.add_argument("-qoi_seed", type=int, default=-1,
-                        action="store", help="Seed for random generator")
-
-
-def main():
-    import argparse
-    from pdelib.SField import SField
-    SField.Init()
-
-    parser = argparse.ArgumentParser(add_help=True)
-    addExtraArguments(parser)
-    mimc.MIMCRun.addOptionsToParser(parser)
-    sf = SField()
-
-    mimcRun = mimc.MIMCRun(**vars(parser.parse_known_args()[0]))
-    if mimcRun.params.qoi_seed >= 0:
-        np.random.seed(mimcRun.params.qoi_seed)
-    fnItrDone = None
-    if mimcRun.params.db:
-        if hasattr(mimcRun.params, "db_user"):
-            db = mimcdb.MIMCDatabase(user=mimcRun.params.db_user,
-                                     host=mimcRun.params.db_host)
-        else:
-            db = mimcdb.MIMCDatabase(host=mimcRun.params.db_host)
-        run_id = db.createRun(mimc_run=mimcRun,
-                              tag=mimcRun.params.db_tag)
-        fnItrDone = lambda *a: db.writeRunData(run_id, mimcRun, *a)
-
-    mimcRun.setFunctions(fnSampleLvl=lambda *a: SamplePDE(sf, mimcRun, *a),
-                         fnItrDone=fnItrDone)
-
-    try:
-        mimcRun.doRun()
-    except:
-        if mimcRun.params.db:
-            db.markRunFailed(run_id)
-        raise   # If you don't want to raise, make sure the following code is not executed
-
-    SField.Final()
-    if mimcRun.params.db:
-            db.markRunSuccessful(run_id)
-    return mimcRun.data.calcEg()
 
 
 def SamplePDE(sf, run, moments, mods, inds, M):
@@ -76,4 +12,10 @@ def SamplePDE(sf, run, moments, mods, inds, M):
     return M, psums, time.time() - timeStart
 
 if __name__ == "__main__":
-    main()
+    from pdelib.SField import SField
+    SField.Init()
+    with SField() as sf:
+        import mimclib.test
+        mimclib.test.RunStandardTest(fnSampleLvl=lambda *a: SamplePDE(sf, *a))
+
+    SField.Final()
