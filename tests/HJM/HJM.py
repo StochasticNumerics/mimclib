db0347e51b12c01c6b607b21d06347edcc980dad
Juho Häppölä <juho.happola@iki.fi>
2016-05-07 21:38:19 +0300

    fig 3 replot

--- tests/HJM/HJM.py
+++ tests/HJM/HJM.py
@@ -877,11 +877,11 @@ def testFourierConvergence():
 
 rates =[1.5,3]
 char = 'f'
-onum = 2
-ret=2
+onum = 1
+ret=1
 
-N=10
-sample=[testcude([[2,foo] for foo in range(N)],ret=ret) for m in range(50)]
+N=9
+sample=[testcude([[9,foo] for foo in range(N)],ret=ret) for m in range(50)]
 plt.figure()
 plt.loglog([2**foo for foo in range(N-1)],np.mean(np.abs(np.diff(sample,axis=1)),axis=0),'b-')
 plt.loglog([2**foo for foo in range(N-1)],[2**(-1*foo*rates[0]) for foo in range(N-1)],'r-')
