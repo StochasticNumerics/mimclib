8796a4ba448dc8095b358a962cdccf863df78ad7
Abdo Haji-Ali <abdo.haji.ali@gmail.com>
2016-09-11 15:26:59 +0100

    Corrected a few mistakes in the script

--- mimc_install.sh
+++ mimc_install.sh
@@ -26,40 +26,26 @@
 echo -e "First, update existing packages..."
 
 sudo apt-get update
-sudo apt-get upgrade    
+sudo apt-get upgrade
 
 echo -e "Existing packages upgraded."
 echo -e "Installing packages..."
 
-sudo apt-get install git python-pip mysql-server mysql-client libmysqlclient-dev build-essential ipython libpng-dev libfreetype6-dev libxft-dev libpython-dev liblapack-dev libblas-dev gfortran parallel
+sudo apt-get install git python-pip mysql-server mysql-client \
+     libmysqlclient-dev build-essential ipython libpng-dev libfreetype6-dev \
+     libxft-dev libpython-dev liblapack-dev libblas-dev gfortran parallel
 
 echo -e "Done installing aptitude packages."
 
-echo -e "Adding python packages using pip"
-
+echo -e "Making and installing"
 pip install --upgrade pip
-
-# For whatever reason this needs root privileges
-sudo -H pip install --upgrade numpy
-sudo -H pip install --upgrade matplotlib
-sudo -H pip install --upgrade scipy
-sudo -H pip install --upgrade mysql-python
-
-echo -e "Done adding python packages"
-
-cd
-git clone https://github.com/StochasticNumerics/mimclib.git
-cd mimclib
-make
 make pip
-
-echo -e "Creating the mimc database to mySQL.\n You will need to enter mySQL root password."
-python -c 'from mimclib.db import MIMCDatabase; print MIMCDatabase().DBCreationScript();' | mysql -u root -p
 
 echo -e "Adding a user to mySQL:"
 echo -e "CREATE USER '$USER'@'%';\nGRANT ALL PRIVILEGES ON mimc.* TO '$USER'@'%' WITH GRANT OPTION;" | mysql -u root -p
 
+echo -e "Creating the mimc database to mySQL.\n You will need to enter mySQL root password."
+python -c 'from mimclib.db import MIMCDatabase; print MIMCDatabase().DBCreationScript();' | mysql -u $USER
+
 cd tests/gbm
 make
-
-
